17feac37be26cac5b8b46a53144848964f172c25
LiceAwz <liceawazu@gmail.com>
2025-03-04 11:11:10 -0300

    Adicionando funcionalidade de adicionar novo produto

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,40 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Models;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        //colocando para que o App entenda que estamos usando o SQLite
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
             // MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
+
+            
+
         }
     }
 }
